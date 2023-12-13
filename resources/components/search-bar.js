c711cf18a73952616cd9af3a82a8f8516ccaec27
Benjamin <bnjackson951@gmail.com>
2023-12-13 15:56:41 +0000

    Check for empty search inputs, and only search if input is not empty

--- resources/components/search-bar.js
+++ resources/components/search-bar.js
@@ -1,9 +1,11 @@
 const SEARCH_INPUT = document.querySelector('#searchInput');
 
 function googleSearch(searchInput) {
-    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchInput)}`;
-    console.log(googleSearchUrl);
-    window.location.href = googleSearchUrl;
+    if (searchInput.trim() !== '') {
+        const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchInput)}`;
+        window.location.href = googleSearchUrl;
+    }
+
 }
 
 export {SEARCH_INPUT, googleSearch};
